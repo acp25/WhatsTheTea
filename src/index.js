6581a03678606ee3398d12c771634bc782ba1817
Keith Watford <keith.l.watford@gmail.com>
2021-08-12 10:48:52 -0400

    drafted the index in source

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-// Decide on and import a CSS framework
 
 ReactDOM.render(<App />, document.getElementById('root'));
