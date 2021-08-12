9928bef59e319e20c76873fa0e8d6308927279c1
Keith Watford <keith.l.watford@gmail.com>
2021-08-12 10:43:32 -0400

    more basic file stuff

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+// Decide on and import a CSS framework
+
+ReactDOM.render(<App />, document.getElementById('root'));
