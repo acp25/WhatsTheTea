e04e64060eefd7596ae7940a72b5a4202ca30264
Keith Watford <keith.l.watford@gmail.com>
2021-08-13 12:12:44 -0400

    organized a little bit but Randy and i had the same idea. will resolved later

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Restaurants from './components/pages/restaurants';
+import Menu from './components/pages/menu';
+
+import Header from './components/header';
+
 
 // TODO: Add a comment explaining what this function is doing
 // This function is a functional component that helps us split the UI into distinct parts.
 // In this case, we are returning another component, <HelloReact/> from it.
 function App() {
-  return <Restaurants />;
+  return( 
+    <>
+  <Header />
+  {/* <Restaurants /> */}
+  <Menu />
+  </>
+  ) 
 }
 
 export default App;
