f2ef567f0aa5bf3a75d5301b87d3a072079038b5
Keith Watford <keith.l.watford@gmail.com>
2021-08-13 14:13:33 -0400

    attempted some formatting

--- client/src/components/pages/Nav.js
+++ client/src/components/pages/Nav.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './../../styles/app.css' 
 
 export default function Nav(props) {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light pb-0">
-            <div className="container-fluid ps-4 align-items-end">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light pb-0" >
+            <div className="container-fluid ps-4 align-items-end" id="nav">
                 <Link to="/" className="navbar-brand fs-1">
-                    <p className="title">What's The Tea? <i className="fas fa-mug-hot"></i></p>
+                    <p className="title">What's The Tea? <i className=" mug-color fas fa-mug-hot "></i></p>
                 </Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse justify-content-end align-items-end" id="navbarNav">
-                    <ul className="navbar-nav pe-3">
+                    <ul className="navbar-nav pe-3 is-justify-content-center">
                         <li className="nav-item">
                             <Link to="/account"  className="nav-link p-3" href="#account">
                                 Account
