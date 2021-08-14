7e17aa5c1083f49419e688cc4ebe05a28c91eb34
Randy Chou <Randychou2020@gmail.com>
2021-08-13 20:19:03 -0400

    functioning client querries

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,59 @@
 import React from 'react';
-import Menu from './components/pages/menu';
-import FoodItem from './components/pages/foodItem';
-import Header from './components/header';
-
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { StoreProvider } from './utils/GlobalState';
 
 import Account from './components/pages/AccountTemp';
 import Address from './components/pages/AddressTemp';
 import Login from './components/pages/LoginTemp';
 import Nav from './components/pages/Nav';
 import Restaurants from './components/pages/Restaurants';
+import Test from './components/pages/Test';
+
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-
   return (
-    <Router>
-      <div>
-        <Nav/>
-        <Switch>
-          <Route exact path="/" component={Restaurants} />
-          <Route exact path="/account" component={Account} />
-          <Route exact path="/address" component={Address} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/menu/:restaurantId" component={Login} />
-        </Switch>
-      </div>
-      {/* <Header /> */}
-  {/* <Restaurants /> */}
-  {/* <Menu /> */}
-  {/* <FoodItem /> */}
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Nav />
+            <Switch>
+              <Route exact path="/" component={Restaurants} />
+              <Route exact path="/account" component={Account} />
+              <Route exact path="/address" component={Address} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/test" component={Test} />
+              <Route exact path="/menu/:restaurantId" component={Test} />
+            </Switch>
+          </StoreProvider>
+        </div>
+      </Router>
+    </ApolloProvider>
   );
 }
 
