a34545597bef771ed57258de8e4eaac1aa637bb1
Alex Bright <abrighttt@gmail.com>
2021-08-13 20:26:56 -0400

    starting account settings page

--- client/src/components/pages/AccountTemp.js
+++ client/src/components/pages/AccountTemp.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Account(props) {
     return (
-        <section>
-           <h1>This is the Account Settings Page</h1>
+        <section class="section is-large">
+        <div class="container">
+           <center><h1 class="title">Account Settings</h1></center>
+        </div>
+        <center><div class="image is-128x128">  
+           <img class="is-rounded" src="https://bulma.io/images/placeholders/128x128.png"></img>
+        </div></center>
+        <center><div class="buttons">
+            <button class="button is-link">Change Profile Pic</button>
+        </div></center>
         </section>
+       
     );
 }
+
