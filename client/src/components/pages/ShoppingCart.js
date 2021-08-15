b92b767cd50a846f13ebf9cb2e6987cb961e5325
Keith Watford <keith.l.watford@gmail.com>
2021-08-15 09:50:50 -0400

    draft of a shopping cart. needs the logic and im hoping the button on this page can take user to stripe (not set up)

--- client/src/components/pages/ShoppingCart.js
+++ client/src/components/pages/ShoppingCart.js
@@ -1,24 +1,54 @@
 import React, { useState } from "react";
 import "./../../styles/float-btn.css";
+import "./../../styles/app.css";
 
-export default function Restaurants(props) {
-  //     const [input, setInput] = useState('');
+export default function ShoppingCart() {
 
-  //     const handleSubmit = (e) => {
-  //         // e.preventDefault();
-  //     }
-
-  //     props.onSubmit({
-  //         text: input,
-  //     });
-
-  //     setInput('')
-  // }
+  const foodItem = "Bomb-Food"
+  const price = 1.57
+  const quantity = 2;
+  const linePrice = (price)*(quantity);
   return (
     <>
-      {/* <a href="#"> */}
-      <section className="card"></section>
-      {/* </a> */}
+      <h1 className="">Your Cart</h1>
+      <section className="card-table">
+      <table className="table-rounded table is-hoverable is-bordered text-shadow ">
+        {/* <thead className=" is-narrow "> */}
+          <tr>
+            <th>
+              <i className="fas fa-utensils"> </i>
+            </th>
+            <th>
+              <abbr title="Quantity">Qty</abbr>
+            </th>
+            <th>
+              <i className="fas fa-dollar-sign"> </i>
+            </th>
+          </tr>
+        {/* </thead> */}
+        <tbody className="">
+          <tr >
+            <td >{foodItem}</td>
+            <td>{quantity}</td>
+            <td>${linePrice}</td>
+          </tr>
+          <tr >
+            <td >{foodItem}</td>
+            <td>{quantity}</td>
+            <td>${linePrice}</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p className="text-shadow">
+      <strong>Subtotal:</strong> (Somehow grab all items, multiply each qty by price and add together.)
+      </p>
+      </section>
+
+      <a href="#" class="float">
+        <i className="far fa-credit-card my-float">
+          <br/>Pay</i>
+    </a>
     </>
   );
 }
